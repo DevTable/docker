90102f31d8a7098baa459743ddc7120efc28bc64
Fernando <fermayo@gmail.com>
2014-05-14 11:55:12 -0400

    fix uid/gid mapping

--- graph/graph.go
+++ graph/graph.go
@@ -156,66 +156,6 @@ func (graph *Graph) Create(layerData archive.ArchiveReader, containerID, contain
 	return img, nil
 }
 
-func xlateOneFile(path string, finfo os.FileInfo, hUid, cUid, size int64) error {
-	uid := int64(finfo.Sys().(*syscall.Stat_t).Uid)
-	gid := int64(finfo.Sys().(*syscall.Stat_t).Gid)
-	mode := finfo.Mode()
-
-	if uid >= cUid && uid < cUid+size {
-		newUid := (uid - cUid) + hUid
-		newGid := (gid - cUid) + hUid
-		if err := os.Lchown(path, int(newUid), int(newGid)); err != nil {
-			return fmt.Errorf("Cannot chown %s: %s", path, err)
-			// Let's keep going
-		}
-		if err := os.Chmod(path, mode); err != nil {
-			return fmt.Errorf("Cannot chmod %s: %s", path, err)
-			// Let's keep going
-		}
-	}
-
-	return nil
-}
-
-func xlateUidsRecursive(base string, hUid, cUid, size int64) error {
-	f, err := os.Open(base)
-	if err != nil {
-		return err
-	}
-
-	list, err := f.Readdir(-1)
-	f.Close()
-	if err != nil {
-		return err
-	}
-
-	for _, finfo := range list {
-		path := filepath.Join(base, finfo.Name())
-		if finfo.IsDir() {
-			xlateUidsRecursive(path, hUid, cUid, size)
-		}
-		if err := xlateOneFile(path, finfo, hUid, cUid, size); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func xlateUids(root string) error {
-	if err := xlateUidsRecursive(root, 100000, 0, 100000); err != nil {
-		return err
-	}
-	finfo, err := os.Stat(root)
-	if (err != nil) {
-		return err
-	}
-	if err := xlateOneFile(root, finfo, 100000, 0, 100000); err != nil {
-		return err
-	}
-	return nil
-}
-
 // Register imports a pre-existing image into the graph.
 // FIXME: pass img as first argument
 func (graph *Graph) Register(jsonData []byte, layerData archive.ArchiveReader, img *image.Image) (err error) {
@@ -266,9 +206,6 @@ func (graph *Graph) Register(jsonData []byte, layerData archive.ArchiveReader, i
 	defer graph.driver.Put(img.ID)
 	img.SetGraph(graph)
 	if err := image.StoreImage(img, jsonData, layerData, tmp, rootfs); err != nil {
-		return err
-	}
-	if err := xlateUids(rootfs); err != nil {
 		return err
 	}
 	// Commit
